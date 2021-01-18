4f780438bd0bd0afa922f1a73f65a0c70cc67c56
zbqmgldjfh <60593969+zbqmgldjfh@users.noreply.github.com>
2021-01-18 12:15:38 +0900

    오탈자 수정
    class뒤에 ;가 빠져있었음....

--- main.cpp
+++ main.cpp
@@ -3,37 +3,37 @@
 
 using namespace std;
 
-class cPlayer{
+class cPlayer {
 public:
     int x, y;
-    cPlayer(int width) {x = width / 2; y = 0;} // 위치는 위쪽중앙에서 시작
-}
+    cPlayer(int width) { x = width / 2; y = 0; } // 위치는 위쪽중앙에서 시작
+};
 
-class cLane{
+class cLane {
 private:
-    deque<bool> // lane을 연속의 dot로 표현할것, dot는 true, false로 결정
+    deque<bool> cars;// lane을 연속의 dot로 표현할것, dot는 true, false로 결정
 public:
     cLane(int width)
     {
-        for(int i(0); i < width; i++)
+        for (int i(0); i < width; i++)
             cars.push_front(false); // deque는 앞뒤에서 추출가능, flase는 차가 없는 상태
     }
 
     void Move() // 자동차 움직이기
     {
-            if(rand() % 10 === 1) // 생성한 렌덤수가 1이면 mave car
-                cars.push_front(true);
-            else
-                cars.push_front(false);
+        if (rand() % 10 == 1) // 생성한 렌덤수가 1이면 mave car
+            cars.push_front(true);
+        else
+            cars.push_front(false);
     }
     // 000100이면 cars[3]은 true
     bool CheckPos(int pos) { return cars[pos]; } // car의 위치 
 
-}
+};
 
 class cGame {
 private:
-    bool quit; 
+    bool quit;
 public:
     void Draw()
     {
@@ -52,30 +52,30 @@ public:
 
     void Run()
     {
-        while(!quit)
+        while (!quit)
         {
             Input();
             Draw();
             Logic();
         }
     }
-}
+};
 
 int main()
 {
     cLane lane1(20);
-    for(int i = 0; i < 20; i++)
+    for (int i = 0; i < 20; i++)
     {
-        if(lane1.CheckPos(i))
+        if (lane1.CheckPos(i))
             cout << "#";
         else
             cout << "0";
     }
     lane1.Move();
     cout << endl;
-    for(int i = 0; i < 20; i++)
+    for (int i = 0; i < 20; i++)
     {
-        if(lane1.CheckPos(i))
+        if (lane1.CheckPos(i))
             cout << "#";
         else
             cout << "0";
@@ -85,9 +85,9 @@ int main()
     lane1.Move();
     lane1.Move();
     cout << endl;
-    for(int i = 0; i < 20; i++)
+    for (int i = 0; i < 20; i++)
     {
-        if(lane1.CheckPos(i))
+        if (lane1.CheckPos(i))
             cout << "#";
         else
             cout << "0";
